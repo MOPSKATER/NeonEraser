bef54bf3f3c58330ed2b0cff1f2118879a45869d
MOPSKATER <josua.w@gmx.de>
2023-01-05 17:13:53 +0100

    Projektdateien hinzufügen.

---
+++ NeonEraser/AssemblyInfo1.cs
@@ -0,0 +1,22 @@
+using MelonLoader;
+using System.Runtime.InteropServices;
+
+// In Projekten im SDK-Stil wie dem vorliegenden, bei dem verschiedene Assemblyattribute
+// üblicherweise in dieser Datei definiert wurden, werden diese Attribute jetzt während
+// der Builderstellung automatisch hinzugefügt und mit Werten aufgefüllt, die in den
+// Projekteigenschaften definiert sind. Informationen dazu, welche Attribute einbezogen
+// werden und wie dieser Prozess angepasst werden kann, finden Sie unter https://aka.ms/assembly-info-properties.
+
+
+// Wenn "ComVisible" auf FALSE festgelegt wird, sind die Typen in dieser Assembly
+// für COM-Komponenten nicht sichtbar. Wenn Sie von COM aus auf einen Typ in dieser
+// Assembly zugreifen müssen, legen Sie das ComVisible-Attribut für den betreffenden
+// Typ auf TRUE fest.
+
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM
+// bereitgestellt wird.
+
+[assembly: Guid("ca4e48c3-6925-4a82-83c7-60d107138c0d")]
+[assembly: MelonInfo(typeof(NeonEraser.Eraser), "NeonEraser", "1.0.0", "MOPSKATER")]
---
+++ NeonEraser/Eraser.cs
@@ -0,0 +1,63 @@
+﻿using Steamworks;
+using System.Reflection;
+using UnityEngine.InputSystem;
+
+namespace NeonEraser
+{
+    public class Eraser : MelonLoader.MelonMod
+    {
+        private const int NEWTIMEUPLOAD = 600000;
+        private const long NEWTIMELOCAL = 600000000;
+
+        private readonly CallResult<LeaderboardFindResult_t> findLeaderboardForUpload =
+            CallResult<LeaderboardFindResult_t>.Create
+            (new CallResult<LeaderboardFindResult_t>.APIDispatchDelegate
+                (OverrideScore));
+
+        
+        private static CallResult<LeaderboardScoreUploaded_t> leaderboardScoreUploadedResult2;
+
+
+        public override void OnUpdate()
+        {
+            if (!Keyboard.current.tKey.wasPressedThisFrame) return;
+
+            if (leaderboardScoreUploadedResult2 == null)
+            {
+                typeof(LeaderboardIntegrationSteam).GetMethod("Initialize", BindingFlags.NonPublic | BindingFlags.Static).Invoke(null, new object[0]);
+                leaderboardScoreUploadedResult2 = (CallResult<LeaderboardScoreUploaded_t>)typeof(LeaderboardIntegrationSteam).
+                    GetField("leaderboardScoreUploadedResult2", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null);
+            }
+
+            GameDataManager.levelStats["GRID_EXTERMINATOR"]._timeBestMicroseconds = NEWTIMELOCAL;
+            GameDataManager.SaveGame();
+            Erase("GRID_EXTERMINATOR");
+        }
+
+        private void EraseAll()
+        {
+            GameData gameData = Singleton<Game>.Instance.GetGameData();
+            foreach (var campaign in gameData.campaigns)
+                foreach (var missionData in campaign.missionData)
+                    foreach (var level in missionData.levels)
+                        Erase(level.levelID);
+        }
+
+        private void Erase(string boardName)
+        {
+            SteamAPICall_t hAPICall = SteamUserStats.FindOrCreateLeaderboard(boardName, ELeaderboardSortMethod.k_ELeaderboardSortMethodAscending, ELeaderboardDisplayType.k_ELeaderboardDisplayTypeTimeMilliSeconds);
+            findLeaderboardForUpload.Set(hAPICall, null);
+        }
+
+        private static void OverrideScore(LeaderboardFindResult_t pCallback, bool bIOFailure)
+        {
+            if (pCallback.m_bLeaderboardFound != 1 || bIOFailure)
+            {
+                UnityEngine.Debug.LogError("No leaderboard fetched!");
+                return;
+            }
+            SteamAPICall_t hAPICall = SteamUserStats.UploadLeaderboardScore(pCallback.m_hSteamLeaderboard, ELeaderboardUploadScoreMethod.k_ELeaderboardUploadScoreMethodForceUpdate, NEWTIMEUPLOAD, null, 0);
+            leaderboardScoreUploadedResult2.Set(hAPICall, null);
+        }
+    }
+}
