710ca7f7fc8dae94c167367b21519e42f64423a1
MOPSKATER <josua.w@gmx.de>
2023-01-05 20:49:40 +0100

    Basic functionality works

--- NeonEraser/Eraser.cs
+++ NeonEraser/Eraser.cs
@@ -1,5 +1,6 @@
 ﻿using Steamworks;
 using System.Reflection;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace NeonEraser
@@ -9,43 +10,39 @@ namespace NeonEraser
         private const int NEWTIMEUPLOAD = 600000;
         private const long NEWTIMELOCAL = 600000000;
 
-        private readonly CallResult<LeaderboardFindResult_t> findLeaderboardForUpload =
-            CallResult<LeaderboardFindResult_t>.Create
-            (new CallResult<LeaderboardFindResult_t>.APIDispatchDelegate
-                (OverrideScore));
-
         private readonly CallResult<LeaderboardFindResult_t> findLeaderboardForUploadGlobal =
             CallResult<LeaderboardFindResult_t>.Create
             (new CallResult<LeaderboardFindResult_t>.APIDispatchDelegate
                 (AdjustGlobal));
-
-
         private static CallResult<LeaderboardScoreUploaded_t> leaderboardScoreUploadedResult2;
+
+
+        private static readonly MethodInfo OnLeaderboardScoreUploaded2Info = typeof(LeaderboardIntegrationSteam).
+            GetMethod("OnLeaderboardScoreUploaded2", BindingFlags.NonPublic | BindingFlags.Static);
+
+        private static List<CallResult<LeaderboardFindResult_t>> findResults = new List<CallResult<LeaderboardFindResult_t>>();
+        private static List<CallResult<LeaderboardScoreUploaded_t>> upload = new List<CallResult<LeaderboardScoreUploaded_t>>();
 
 
         public override void OnUpdate()
         {
-            if (Keyboard.current.uKey.wasPressedThisFrame)
-            {
-                SteamAPICall_t hAPICall = SteamUserStats.FindOrCreateLeaderboard("GlobalNeonRankings", ELeaderboardSortMethod.k_ELeaderboardSortMethodAscending, ELeaderboardDisplayType.k_ELeaderboardDisplayTypeTimeMilliSeconds);
-                findLeaderboardForUploadGlobal.Set(hAPICall, null);
-            }
 
             if (Keyboard.current.tKey.wasPressedThisFrame)
             {
-
+                findResults.Clear();
+                upload.Clear();
                 if (leaderboardScoreUploadedResult2 == null)
                 {
                     typeof(LeaderboardIntegrationSteam).GetMethod("Initialize", BindingFlags.NonPublic | BindingFlags.Static).Invoke(null, new object[0]);
                     leaderboardScoreUploadedResult2 = (CallResult<LeaderboardScoreUploaded_t>)typeof(LeaderboardIntegrationSteam).
                         GetField("leaderboardScoreUploadedResult2", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null);
                 }
-                EraseAll();
             }
 
-            if (Keyboard.current.xKey.wasPressedThisFrame)
-            { 
-                SteamAPI.RunCallbacks();
+            if (Keyboard.current.uKey.wasPressedThisFrame)
+            {
+                SteamAPICall_t hAPICall = SteamUserStats.FindOrCreateLeaderboard("GlobalNeonRankings", ELeaderboardSortMethod.k_ELeaderboardSortMethodAscending, ELeaderboardDisplayType.k_ELeaderboardDisplayTypeTimeMilliSeconds);
+                findLeaderboardForUploadGlobal.Set(hAPICall, null);
             }
         }
 
@@ -60,30 +57,44 @@ namespace NeonEraser
 
         private void Erase(string levelID)
         {
+            SteamAPICall_t hAPICall = SteamUserStats.FindOrCreateLeaderboard(levelID, ELeaderboardSortMethod.k_ELeaderboardSortMethodAscending, ELeaderboardDisplayType.k_ELeaderboardDisplayTypeTimeMilliSeconds);
+            var result = CallResult<LeaderboardFindResult_t>.Create
+            (new CallResult<LeaderboardFindResult_t>.APIDispatchDelegate
+                (OverrideScore));
+            result.Set(hAPICall);
+            findResults.Add(result);
+
             GameDataManager.levelStats[levelID]._timeBestMicroseconds = NEWTIMELOCAL;
             GameDataManager.levelStats[levelID]._timeLastMicroseconds = NEWTIMELOCAL;
             GameDataManager.SaveGame();
-
-            SteamAPICall_t hAPICall = SteamUserStats.FindOrCreateLeaderboard(levelID, ELeaderboardSortMethod.k_ELeaderboardSortMethodAscending, ELeaderboardDisplayType.k_ELeaderboardDisplayTypeTimeMilliSeconds);
-            findLeaderboardForUpload.Set(hAPICall, null);
         }
 
         private static void OverrideScore(LeaderboardFindResult_t pCallback, bool bIOFailure)
         {
             if (pCallback.m_bLeaderboardFound != 1 || bIOFailure)
             {
-                UnityEngine.Debug.LogError("No leaderboard fetched!");
+                Debug.LogError("No leaderboard fetched!");
                 return;
             }
+
             SteamAPICall_t hAPICall = SteamUserStats.UploadLeaderboardScore(pCallback.m_hSteamLeaderboard, ELeaderboardUploadScoreMethod.k_ELeaderboardUploadScoreMethodForceUpdate, NEWTIMEUPLOAD, null, 0);
-            leaderboardScoreUploadedResult2.Set(hAPICall, null);
+            var result = CallResult<LeaderboardScoreUploaded_t>.Create
+                (new CallResult<LeaderboardScoreUploaded_t>.APIDispatchDelegate
+                (OnLeaderboardScoreUploaded2));
+            result.Set(hAPICall);
+            upload.Add(result);
+        }
+
+        private static void OnLeaderboardScoreUploaded2(LeaderboardScoreUploaded_t pCallback, bool bIOFailure)
+        {
+            OnLeaderboardScoreUploaded2Info.Invoke(null, new object[] { pCallback, bIOFailure });
         }
 
         private static void AdjustGlobal(LeaderboardFindResult_t pCallback, bool bIOFailure)
         {
             if (pCallback.m_bLeaderboardFound != 1 || bIOFailure)
             {
-                UnityEngine.Debug.LogError("No leaderboard fetched!");
+                Debug.LogError("No leaderboard fetched!");
                 return;
             }
             LeaderboardScoreCalculation.GetGlobalNeonScoreUploadData(out int nScore, out int num);
